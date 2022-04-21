70d5572cf0e937c376b92147aea2715690923e3d
JustineJC <99407404+JustineJC@users.noreply.github.com>
2022-04-21 15:41:13 +0530

    Adding few comments for easy navigation

--- payMoney/src/com/transaction/main/controllerClass.java
+++ payMoney/src/com/transaction/main/controllerClass.java
@@ -4,9 +4,18 @@ import com.transaction.model.transaction;
 
 public class controllerClass {
     public static void main(String[] args)throws Exception {
+        //This class has both payMoney and Currency denomination case in one class.
+        
+        //obj is an object created for calculating the daily target achieved during daily transactions.
         transaction obj = new transaction();
+        
+        //object note is created for calculating the count of available denominators in a currency.
         denominator note = new denominator();
+        
+        //calculate the daily transcation target given
         obj.transactionCounter();
+        
+        //calculate the count of denominations in a given currency.
         note.denominatorCounter();
         
     }
